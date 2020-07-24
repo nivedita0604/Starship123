f346827db2c0128cbc38ef667bb3a49f40b57e79
nivedita0604 <54851166+nivedita0604@users.noreply.github.com>
2020-07-25 00:56:02 +0530

    Updates

---
+++ precache-manifest.0722448169a0c9b403a8866a91db344a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416ebf6c3ed3d389ed38bc5286c2b143",
+    "url": "/Starship123/index.html"
+  },
+  {
+    "revision": "86da6d9c6622aafdddb2",
+    "url": "/Starship123/static/css/main.101ddd68.chunk.css"
+  },
+  {
+    "revision": "6701e78002780f0a5996",
+    "url": "/Starship123/static/js/2.a02577f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Starship123/static/js/2.a02577f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86da6d9c6622aafdddb2",
+    "url": "/Starship123/static/js/main.e56458a3.chunk.js"
+  },
+  {
+    "revision": "b7203a58b24f2a0c0582",
+    "url": "/Starship123/static/js/runtime-main.285bd61b.js"
+  },
+  {
+    "revision": "25a7d55c1961500ba79b534a69a8b1f4",
+    "url": "/Starship123/static/media/spaces.25a7d55c.png"
+  }
+]);
